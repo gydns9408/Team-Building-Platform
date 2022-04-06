dc67fbfab9f2c1e1327d93e4c340b50035f6706f
canoe <ehgus430880@gmail.com>
2022-04-07 07:06:38 +0900

    Apply prisma to project

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,46 @@
 import Head from "next/head";
-import Link from "next/Link"
+import Link from "next/Link";
 import styles from "../styles/Home.module.css";
 
-export default function MainPage(){
-  return(
-      <>
+export default function MainPage() {
+  return (
+    <>
       <h1>메인 페이지</h1>
       <h2 className={styles.title}>
         <Link href="screen/loginpage">
           <a>로그인</a>
         </Link>
-        </h2>
-        <h2 className={styles.title}>
+      </h2>
+      <h2 className={styles.title}>
         <Link href="screen/registerpage">
           <a>회원가입</a>
         </Link>
-        </h2>
-        <h2 className={styles.title}>
+      </h2>
+      <h2 className={styles.title}>
         <Link href="screen/competitionsearchpage">
           <a>대회 탐색</a>
         </Link>
-        </h2>
-        <h2 className={styles.title}>
+      </h2>
+      <h2 className={styles.title}>
         <Link href="screen/teamsearchpage">
           <a>팀 탐색</a>
         </Link>
-        </h2>
-        <h2 className={styles.title}>
+      </h2>
+      <h2 className={styles.title}>
         <Link href="screen/partnersearchpage">
           <a>파트너 탐색</a>
         </Link>
-        </h2>
-        <h2 className={styles.title}>
+      </h2>
+      <h2 className={styles.title}>
         <Link href="screen/guidepage">
           <a>가이드</a>
         </Link>
-        </h2>
-        <h2 className={styles.title}>
+      </h2>
+      <h2 className={styles.title}>
         <Link href="screen/profilepage">
           <a>프로필</a>
         </Link>
-        </h2>
-      </>
-  )
+      </h2>
+    </>
+  );
 }
---
+++ pages/screen/loginpage.js
@@ -0,0 +1,49 @@
+import Link from "next/Link";
+import styles from "../../styles/Home.module.css";
+
+export default function LoginPage() {
+  return (
+    <>
+      <h1>로그인</h1>
+      <div>
+        <form id="login-form">
+          <ul className="login-items">
+            <li>
+              <label htmlFor="email">Email </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="입력하세요"
+                className="login-class"
+              />
+            </li>
+            <li>
+              <label htmlFor="password">비밀번호 </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="입력하세요"
+                className="login-class"
+              />
+            </li>
+            <li>
+              <button type="submit" className="primary">
+                로그인
+              </button>
+              <Link href="registerpage">
+                <button>회원가입</button>
+              </Link>
+            </li>
+          </ul>
+        </form>
+      </div>
+      <h2 className={styles.title}>
+        <Link href="/">
+          <a>메인으로</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
