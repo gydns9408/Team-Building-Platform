dbb3dea5032b3a522e792ba87dcf366771d36d14
gydns9408 <gydns9408@naver.com>
2022-04-21 01:31:54 +0900

    dd

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,21 @@
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
+import PCard from "../../components/profilecard";
 
 export default function ProfilePage() {
   return (
     <>
       <h1>프로필</h1>
+      <PCard
+        myPhoto="1"
+        myField="1"
+        explan="안녕하세요."
+        name="오박사"
+        address="태초마을"
+        contactAddress="010 1234 4321"
+        skillStack={[1, 2, 3, 4]}
+        myEstimate={["AAA", "AAA", "AAA"]}
+      />
       <h2 className={styles.title}>
         <Link href="/">
           <a>메인으로</a>
