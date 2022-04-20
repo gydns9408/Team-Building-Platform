792c3e07d3f399c41c8a66ae0eb27e68cf75d84f
gydns9408 <gydns9408@naver.com>
2022-04-21 01:31:54 +0900

    dd

--- components/profilecard.js
+++ components/profilecard.js
@@ -1,38 +1,41 @@
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import styles from "../styles/Home.module.css";
 import Image from "next/image";
-
 const imgType = { png: ".png" };
-
 var simbolImageAddress = "/asset/simbol";
 var skillStackAddress = "/asset/skillStack";
+var profilePhotoAddress = "/asset/profilePhoto";
+var estimateAddress = "/asset/estimate_";
 
-const Card = (props) => {
-  let tagsPrint = "";
+const Pcard = (props) => {
+  
 
   const photosize = 50
-  let i = 0;
-  for (; i < props.tags.length; i++) {
-    tagsPrint += props.tags[i] + " ";
-  }
 
   return (
     <div>
-      {props.participants}명
+      <Image
+          src={`${profilePhotoAddress}${props.myPhoto}${imgType.png}`}
+          alt="Picture of me"
+          width={photosize}
+          height={photosize}
+        />
       <header>
         <Image
-          src={`${simbolImageAddress}${props.field}${imgType.png}`}
+          src={`${simbolImageAddress}${props.myField}${imgType.png}`}
           alt="Picture of the author"
           width={photosize}
           height={photosize}
         />
-        &nbsp;
-        {props.maintitle}
       </header>
-      <header>{props.subtitle}</header>
       <header>{props.explan}</header>
-      <header>{tagsPrint}</header>
+      <header>개인정보</header>
+      <header>{props.name}</header>
+      <header>{props.address}</header>
+      <header>{props.contactAddress}</header>
+      <header>기술스택</header>
       <header>
         <Image
           src={`${skillStackAddress}${props.skillStack[0]}${imgType.png}`}
@@ -59,9 +62,29 @@ const Card = (props) => {
           height={photosize}
         />
       </header>
-      상금 : {props.prize}원
+      <header>타인평가</header>
+      <header>
+        <Image
+          src={`${estimateAddress}${props.myEstimate[0]}${imgType.png}`}
+          alt="이미지"
+          width={photosize}
+          height={photosize}
+        />
+        <Image
+          src={`${estimateAddress}${props.myEstimate[1]}${imgType.png}`}
+          alt="이미지"
+          width={photosize}
+          height={photosize}
+        />
+        <Image
+          src={`${estimateAddress}${props.myEstimate[2]}${imgType.png}`}
+          alt="이미지"
+          width={photosize}
+          height={photosize}
+        />
+        </header>
     </div>
   );
 };
 
-export default Card;
+export default Pcard;
