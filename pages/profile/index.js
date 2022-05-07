843415cb41e844cfab6d79e94b35a0d6b58f91e7
gydns9408 <gydns9408@naver.com>
2022-05-07 19:00:44 +0900

    usestate추가

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
 import css from "styled-jsx/css";
-import PCard from "../../components/Card/ProfileCard";
+import React, { useState } from 'react';
+import PCard from "../../components/Card/profilecard";
 
 const style = css`
   .test {
@@ -12,18 +13,27 @@ const style = css`
 `;
 
 export default function ProfilePage() {
+
+  let [myPhotoVar, myPhotoVarHandler] = useState("1")
+  let [myFieldVar, myFieldVarHandler]  = useState("1")
+  let [explanVar, explanVarHandler] = useState("안녕하세요");
+  let [nameVar, nameVarHandler] = useState("오박사");
+  let [addressVar, addressVarHandler] = useState("태초마을");
+  let [contactAddressVar, contactAddressVarHandler] =useState("010 8465 4865");
+  let [skillStackVar, skillStackVarHandler] = useState([1, 2, 3, 4])
+  let [myEstimateVar, myEstimateVarHandler]  = useState(["AAA", "AAA", "AAA"])
   return (
     <>
       <h1>프로필</h1>
       <PCard
-        myPhoto="1"
-        myField="1"
-        explan="안녕하세요."
-        name="오박사"
-        address="태초마을"
-        contactAddress="010 1234 4321"
-        skillStack={[1, 2, 3, 4]}
-        myEstimate={["AAA", "AAA", "AAA"]}
+        myPhoto={myPhotoVar}
+        myField={myFieldVar}
+        explan={explanVar}
+        name={nameVar}
+        address={addressVar}
+        contactAddress={contactAddressVar}
+        skillStack={skillStackVar}
+        myEstimate={myEstimateVar}
       />
       <h2 className={styles.title}>
         <Link href="/">
